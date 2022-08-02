7e9d12aaa252f35ff576c999fc418de7c965f652
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-08-01 23:02:41 -0300

    refactor: refactor cod

--- frontend-projetc1/src/views/Home/Home.jsx
+++ frontend-projetc1/src/views/Home/Home.jsx
@@ -1,36 +1,13 @@
 import "./Home.css";
-import PaletasLista from "components/PaletaLista/PaletasLista";
-import sacola from "assets/icons/sacola.svg";
-import logo from "assets/logo.svg";
+import PaletaLista from "../../components/PaletaLista/PaletasLista";
+import NavBar from 'components/NavBar/NavBar'
 
 function Home() {
   return (
     <div className="Home">
-      <div className="Home__header Header">
-  <div className="row">
-      <div className="Header__logo Logo">
-      <img
-          src={logo}
-          width="70px"
-          alt="Logo El Geladon"
-          className="Logo__icone"
-      />
-      <span className="Logo__titulo"> El Geladon </span>
-      </div>
-      <div className="Header__opcoes Opcoes">
-      <div className="Opcoes__sacola Sacola">
-          <img
-          src={sacola}
-          width="40px"
-          className="Sacola__icone"
-          alt="Sacola de compras"
-          />
-      </div>
-      </div>
-  </div>
-</div>
+      <NavBar />
       <div className="Home__container">
-        <PaletasLista />
+        <PaletaLista />
       </div>
     </div>
   );
