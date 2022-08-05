8cc6cd88d71fe540dba2fd3512a04b5683c46064
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-08-04 23:54:32 -0300

    feat: new resource

--- frontend-projetc1/src/services/PaletaService.js
+++ frontend-projetc1/src/services/PaletaService.js
@@ -1,15 +1,31 @@
 import { Api } from 'helpers/Api';
 const parseResponse = (Response) => Response.json();
 
+const transformPaleta = (paleta) => {
+  const [sabor, recheio] = paleta.sabor.split(" com ");
+
+  return {
+    ...paleta,
+    id: paleta._id,
+    titulo: paleta.sabor,
+    sabor,
+    ...(recheio && { recheio }),
+  };
+};
+
+const parseTransformLista = (response) =>
+  parseResponse(response).then((paletas) => paletas.map(transformPaleta));
+
+
 export const PaletaService = {
   getLista: () =>
-    fetch(Api.paletaLista(), { method: 'GET' }).then(parseResponse),
+    fetch(Api.paletaLista(), { method: 'GET' }).then(parseTransformLista),
   getById: (id) =>
-    fetch(Api.paletaById(id), { method: 'GET' }).then(parseResponse),
+    fetch(Api.paletaById(id), { method: 'GET' }).then(parseTransformLista),
   create: () =>
-    fetch(Api.createPaleta(), { method: 'POST' }).then(parseResponse),
+    fetch(Api.createPaleta(), { method: 'POST' }).then(parseTransformLista),
   updateById: (id) =>
-    fetch(Api.updatePaletaById(id), { method: 'PUT' }).then(parseResponse),
+    fetch(Api.updatePaletaById(id), { method: 'PUT' }).then(parseTransformLista),
   deleteById: (id) =>
-    fetch(Api.deletePaletaById(id), { method: 'DELETE' }).then(parseResponse),
+    fetch(Api.deletePaletaById(id), { method: 'DELETE' }).then(parseTransformLista),
 };
