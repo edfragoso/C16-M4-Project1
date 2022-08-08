46084cb0eb24f0ea0a28ee42119d3897fe479624
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-08-07 22:40:29 -0300

    refactor add create

--- frontend-projetc1/src/views/Home/Home.jsx
+++ frontend-projetc1/src/views/Home/Home.jsx
@@ -1,13 +1,23 @@
 import "./Home.css";
-import PaletaLista from "../../components/PaletaLista/PaletasLista";
+import PaletaLista from "components/PaletaLista/PaletasLista";
+import AdicionaPaletaModal from "components/AdicionaPaletaModal/AdicionaPaletaModal";
 import NavBar from 'components/NavBar/NavBar'
+import { useState } from 'react';
 
 function Home() {
+  
+  const [canShowAdicionaPaletaModal, setCanShowAdicionaPaletaModal] = useState(false);
+  const [paletaParaAdicionar, setPaletaParaAdicionar] = useState();
+
   return (
     <div className="Home">
-      <NavBar />
+      <NavBar createPaleta={() => setCanShowAdicionaPaletaModal(true) } />
       <div className="Home__container">
-        <PaletaLista />
+        <PaletaLista paletaCriada={paletaParaAdicionar} />
+        {
+          canShowAdicionaPaletaModal && <AdicionaPaletaModal closeModal={() => setCanShowAdicionaPaletaModal(false)}
+          onCreatePaleta={(paleta) => setPaletaParaAdicionar(paleta)} />
+        }
       </div>
     </div>
   );
