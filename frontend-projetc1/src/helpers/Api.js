dc122132b14f434dce4b3b2d4ac0c047d4b01f38
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2022-08-07 22:39:41 -0300

    fix: error endPoint

--- frontend-projetc1/src/helpers/Api.js
+++ frontend-projetc1/src/helpers/Api.js
@@ -2,7 +2,7 @@ const PaletaContext = {
     paletaEndpoint: () => `${Api.baseUrl}/paletas`,
     paletaLista: () => `${PaletaContext.paletaEndpoint()}/all-paletas`,
     paletaById: (id) => `${PaletaContext.paletaEndpoint()}/one-paleta/${id}`,
-    createPaleta: () => `${PaletaContext.paletaById()}/create-paleta`,
+    createPaleta: () => `${PaletaContext.paletaEndpoint()}/create-paleta`,
     updatePaletaById: (id) => `${PaletaContext.paletaEndpoint()}/update-paleta/${id}`,
     deletePaletaById: (id) => `${PaletaContext.paletaEndpoint()}/delete-paleta/${id}`,
 };
